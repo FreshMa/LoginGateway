5647e42c5c84536b43946f5e2de15a2772b91e74
FreshMa <kidingworld@gmail.com>
2016-07-03 01:01:28 +0800

    Update login.py

--- login.py
+++ login.py
@@ -1,11 +1,13 @@
 
 # -*-coding:utf-8 -*-
 
+from decimal import *
 from bs4 import BeautifulSoup
 import lxml
 import requests
 import re
 import time
+
 
 s = requests.Session()
 #宿舍网关，该脚本只考虑了这种情况，在图书馆或者教室的网关不一样，所以就不能使用了
@@ -13,13 +15,13 @@ gateAdd = 'http://10.3.8.211'
 def login():
     st = checkOnline2()
     #需要输入自己的帐号和密码
-    login_form={'DDDDD':'上网账号','upass':'上网密码','0MKKey':''}
+    login_form={'DDDDD':'2011212063','upass':'286036','0MKKey':''}
     if st == 'out': 
         s.post(gateAdd,login_form)
+        print u'登录成功，帐号为 %s'%login_form['DDDDD']
         displayInfo()
-        print 'login, the account is %s'%login_form['DDDDD']
     else:
-        print 'already login,the account is %s'%login_form['DDDDD']
+        print u'已处于登录状态，帐号为 %s'%login_form['DDDDD']
         displayInfo()
 
  #检测是否处于登录状态，抓包发现未登录前header的content-length属性的值大于5000，登录之后小于5000，故据此来判断
@@ -48,6 +50,12 @@ def checkOnline2():
         status = 'on'
     return status
 
+#分钟 -> 小时+分钟
+def parseTime(time):
+    minute = time%60
+    hour = time/60
+    return hour,minute
+
 def displayInfo():
     r = s.get(gateAdd)
     html = r.text
@@ -58,9 +66,19 @@ def displayInfo():
     #将该script脚本分行切割，发现第二行包含了我们需要的使用情况信息，故保存
     infoStr = script.splitlines()[1]
     #使用正则匹配出使用时间以及流量等信息
-    m = re.match(r'^time=\'(\d*)\s*\';flow=\'(\d*)\s*\'.*',infoStr)
+    m = re.match(r'^time=\'(\d*)\s*\';flow=\'(\d*)\s*\'.*fee=\'(\d*)\s*\'.*',infoStr)
     time = m.group(1)
     flow = int(m.group(2))
+    fee = int(m.group(3))
+    
+    #时间tuple
+    timeT = parseTime(time)
+    hour = timeT[0]
+    minute = timeT[1]
+
+    #余额
+    fee1 = (float)(fee-fee%100)/10000
+    
     #根据js中的计算方法计算流量，将byte转为Mbyte
     flow0 = flow%1024
     flow1 = flow-flow0
@@ -70,17 +88,21 @@ def displayInfo():
     flow1 = flow1/1024
     #小数部分
     flow0 = flow0/1024
+    #将近多少GB
+    flow3 = round((Decimal(flow1)/Decimal(1024)),2)
 
     #拼接流量字符串
     flow1Str = str(flow1)
     flow0Str = str(flow0)
     flowStr = flow1Str+'.'+flow0Str
-    #输出格式控制（右对齐）
-    flowInfo = 'The used traffic is: %+7s MB'%flowStr
-    timeInfo = 'The used time is: %+10s Min'%time
+    
+    flowInfo = u'使用流量: %+9s MB，将近%4.2fGB'%(flowStr,flow3)
+    timeInfo = u'使用时间: %3d小时%3d分钟'%(hour,minute)
+    feeInfo = u'余额: %9.2f 元'%fee1
 
     print timeInfo
     print flowInfo
+    print feeInfo
     
 login()
 #命令行界面延迟一秒消失
