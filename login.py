4a179c85cb199d1689309581c169836006b4a50c
FreshMa <kidingworld@gmail.com>
2016-07-02 14:03:37 +0800

    添加了剩余流量以及使用时间情况的显示

--- login.py
+++ login.py
@@ -1,20 +1,28 @@
+
+from bs4 import BeautifulSoup
+import lxml
 import requests
+import re
+import time
 
 s = requests.Session()
+#宿舍网关，该脚本只考虑了这种情况，在图书馆或者教室的网关不一样，所以就不能使用了
 gateAdd = 'http://10.3.8.211'
 def login():
-    st = check()
-     #需要输入自己的帐号和密码
+    st = checkOnline()
+    #需要输入自己的帐号和密码
     login_form={'DDDDD':'上网账号','upass':'上网密码','0MKKey':''}
-    if st == 'out':
+    if st == 'out': 
         s.post(gateAdd,login_form)
-        print 'login successfully, the account is %s'%login_form['DDDDD']
+        displayInfo()
+        print 'login, the account is %s'%login_form['DDDDD']
     else:
         print 'already login,the account is %s'%login_form['DDDDD']
+        displayInfo()
 
-def check():
-    #检测是否处于登录状态，抓包发现未登录前header的content-length属性的值大于5000，登录之后小于5000，故据此来判断
+def checkOnline():
     r = s.get(gateAdd)
+     #检测是否处于登录状态，抓包发现未登录前header的content-length属性的值大于5000，登录之后小于5000，故据此来判断
     conLen = r.headers['content-length']
     iLen = int(conLen)
     if iLen<5000:
@@ -23,5 +31,35 @@ def check():
         status = 'out'
     return status
 
+def displayInfo():
+    r = s.get(gateAdd)
+    html = r.text
+    #使用BeautifulSoup来提取网页中的javascript脚本
+    soup = BeautifulSoup(html,'lxml')
+    #只需要script列表中第一个就行
+    script = soup.find_all('script')[0].string
+    #将该script脚本分行切割，发现第二行包含了我们需要的使用情况信息，故保存
+    infoStr = script.splitlines()[1]
+    #使用正则匹配出使用时间以及流量等信息
+    m = re.match(r'^time=\'(\d*)\s*\';flow=\'(\d*)\s*\'.*',infoStr)
+    time = m.group(1)
+    flow = int(m.group(2))
+    #根据js中的计算方法计算流量，将byte转为Mbyte
+    flow0 = flow%1024
+    flow1 = flow-flow0
+    flow0 = flow0*1024
+    flow0 = flow0-flow0%1024
+    #整数部分
+    flow1 = flow1/1024
+    #小数部分
+    flow0 = flow0/1024
+
+    flowStr = 'The used traffic is %d.%d MB'%(flow1,flow0)
+    timeStr = 'The used time is %s Min'%time
+
+    print timeStr
+    print flowStr
+    
 login()
-input()
+#命令行界面延迟一秒消失
+time.sleep(1)
