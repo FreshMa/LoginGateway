10e9b27f3181eab5c1280ca933314b1a4a1d491e
FreshMa <kidingworld@gmail.com>
2016-07-03 20:54:51 +0800

    同步验证方式，多网关支持

--- logout.py
+++ logout.py
@@ -1,16 +1,24 @@
+# -*-coding:utf-8 -*-
 import requests
+from bs4 import BeautifulSoup
 import time
 
 def logout():
+    gateAdd = 'http://10.4.1.2'
+    suf = '/F.htm'
     s = requests.Session()
-    r = s.get('http://10.3.8.211')
-    conLen = r.headers['content-length']
-    iLen = int(conLen)
-    if iLen<5000:
-        requests.get('http://10.3.8.211/F.htm')
-        print 'logout successfully!'
+    r = s.get(gateAdd)
+    r.encoding = 'GBK'
+    html = r.text
+    soup = BeautifulSoup(html,'lxml')
+    title = soup.title.string
+    logStr = u'上网注销窗'
+    if cmp(title,logStr):
+        print u'已处于下线状态，不必执行此操作'
     else:
-        print 'already out'
-
+        requests.get(gateAdd+suf)
+        print u'下线成功'
+        
+        
 logout()
 time.sleep(1)
